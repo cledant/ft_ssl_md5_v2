0fecd7db1ff2614104c694eeb4d1b2acf6a93892
cledant <cledant@student.42.fr>
2019-09-04 00:37:37 +0200

    updated usage + markdown

--- README.md
+++ README.md
@@ -4,4 +4,31 @@ This 42 project aims to implement md5 and sha256 cryptographic hash functions.
 
 ## Compiling
 
-You may compile `ft_ssl` by running `cmake`.
+You may compile `ft_ssl` by running `cmake`.  
+
+## Usage  
+
+Usage: ft_ssl command \[command options\] \[command args\]  
+* Available commands:
+    * md5  
+	* sha256  
+
+## Md5 options
+
+Usage: `md5` \[options\] \[file...\]  
+file... files to digest (default is stdin)  
+-p : echo STDIN to STDOUT and append the checksum to STDOUT  
+-q : quiet mode  
+-r : reverse the format of the output  
+-s : print the checksum of given string  
+-h : display help  
+
+## Sha256 options
+
+Usage: `sha256` \[options\] \[file...\]  
+file... files to digest (default is stdin)  
+-p : echo STDIN to STDOUT and append the checksum to STDOUT  
+-q : quiet mode  
+-r : reverse the format of the output  
+-s : print the checksum of given string  
+-h : display help  
--- packages/args_parsing/private/display.c
+++ packages/args_parsing/private/display.c
@@ -3,16 +3,18 @@
 void
 display_md5_help()
 {
-    puts("Usage: md5\n\n-p : echo STDIN to STDOUT and append the checksum to "
+    puts("Usage: md5 [options] [file...]\nfile... files to digest (default is "
+         "stdin)\n-p : echo STDIN to STDOUT and append the checksum to "
          "STDOUT\n-q : quiet mode\n-r : reverse the format of the output\n-s : "
-         "print the checksum of given string");
+         "print the checksum of given string\n-h : display help");
 }
 
 void
 display_sha256_help()
 {
     puts(
-      "Usage: sha256\n\n-p : echo STDIN to STDOUT and append the checksum to "
+      "Usage: sha256 [options] [file...]\nfile... files to digest (default is "
+      "stdin)\n-p : echo STDIN to STDOUT and append the checksum to "
       "STDOUT\n-q : quiet mode\n-r : reverse the format of the output\n-s : "
-      "print the checksum of given string");
+      "print the checksum of given string\n-h : display help");
 }
